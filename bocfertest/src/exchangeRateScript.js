c8fd54968221aaef5fab49b9b393933fccdb9c32
sfran112 <78272407+sfran112@users.noreply.github.com>
2025-02-08 19:42:12 -0500

    Updated script; now works to find the latest correct date

--- bocfertest/src/exchangeRateScript.js
+++ bocfertest/src/exchangeRateScript.js
@@ -1,6 +1,5 @@
 async function exchangeRate(date, currency) {
     const api = `https://www.bankofcanada.ca/valet/observations/FX${currency}CAD/json?start_date=${date}&end_date=${date}`;
-    console.log("PRESENT")
     try {
         const response = await fetch(api);
         if(!response.status == 200) {
@@ -11,14 +10,30 @@ async function exchangeRate(date, currency) {
         const observations = data.observations;
 
         if(!observations.length){
-            return `No Data for ${currency} on ${date}`;
+            return `False`;
         }
         const rate = observations[0][`FX${currency}CAD`]?.v;
-        return rate ? `Exchange rate on ${date}: ${rate} CAD per ${currency}` : `No rate found for ${currency} on ${date}`;
+        return rate ? `Exchange rate on ${date}: ${rate} CAD per ${currency}` : `False`;
 
     } catch (error) {
         return`Error: ${error.message}`;
     }
 }
 
-exchangeRate("2024-02-03", "USD").then(console.log);
+async function getExchangeRate() {
+    const currency = "USD";
+    const date = new Date("2024-02-07");
+    let item = `False`;
+
+    let formattedDate = date.toISOString().split("T")[0];
+    item = await exchangeRate(formattedDate, currency);
+    while(item == `False`) {
+        date.setDate(date.getDate() - 1);
+        formattedDate = date.toISOString().split("T")[0];
+        item = await exchangeRate(formattedDate, currency);
+    }
+
+    console.log(item);
+}
+
+getExchangeRate();
